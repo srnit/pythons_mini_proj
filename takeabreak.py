853c9c3c9334f8a31ea9c3830b5cb3fd61c00a7d
srnit <ramansudhanshu150@gmail.com>
2017-06-06 22:29:28 +0530

    Automatically open the faveroite videos affter every 2 our you logged in to the computer

---
+++ takeabreak.py
@@ -0,0 +1,9 @@
+import webbrowser;
+import time;
+print("you logged in at"+time.ctime());
+counter=0;
+while(counter<4):
+         time.sleep(2*60*60)
+         webbrowser.open("https://www.youtube.com/watch?v=jzbMxWvnlGw");
+         counter=counter+1;
+
